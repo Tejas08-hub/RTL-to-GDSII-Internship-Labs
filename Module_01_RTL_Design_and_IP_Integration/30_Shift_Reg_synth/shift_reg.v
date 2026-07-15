43ec01428b4b3b68a14be9e65ef2f33c83f7f871
Tejas K <tejask.connect@gmail.com>
2026-07-15 09:13:28 +0000

    8bit shift register design and synthesis id done and added

---
+++ Module_01_RTL_Design_and_IP_Integration/30_Shift_Reg_synth/shift_reg.v
@@ -0,0 +1,12 @@
+module shift_reg(
+	input clk,
+	input rst,
+	input [7:0] data_in,
+	output reg[7:0]q);
+always@(posedge clk or posedge rst)begin
+	if(rst)
+		q<=8'b00000000;
+	else
+		q<={q[6:0],data_in[7]};
+end
+endmodule
