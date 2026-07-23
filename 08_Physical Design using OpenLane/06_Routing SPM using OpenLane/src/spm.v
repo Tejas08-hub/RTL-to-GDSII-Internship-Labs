d52ca90a7580cc1a57e4f1fce3b2c42f3046d7fb
Tejas K <tejask.connect@gmail.com>
2026-07-23 06:31:03 +0000

    Add Routing SPM using OpenLane lab

---
+++ 08_Physical Design using OpenLane/06_Routing SPM using OpenLane/src/spm.v
@@ -0,0 +1,115 @@
+module spm(clk, rst, x, y, p);
+
+    parameter size = 32;
+
+    input clk, rst;
+    input y;
+    input [size-1:0] x;
+    output p;
+
+    wire [size-1:1] pp;
+    wire [size-1:0] xy;
+
+    genvar i;
+
+    // First Carry Save Adder
+    CSADD csa0 (
+        .clk(clk),
+        .rst(rst),
+        .x(x[0] & y),
+        .y(pp[1]),
+        .sum(p)
+    );
+
+    // Remaining Carry Save Adders
+    generate
+        for(i = 1; i < size-1; i = i + 1)
+        begin
+            CSADD csa (
+                .clk(clk),
+                .rst(rst),
+                .x(x[i] & y),
+                .y(pp[i+1]),
+                .sum(pp[i])
+            );
+        end
+    endgenerate
+
+    // Final Transition Comparator
+    TCMP tcmp (
+        .clk(clk),
+        .rst(rst),
+        .a(x[size-1] & y),
+        .s(pp[size-1])
+    );
+
+endmodule
+
+
+//--------------------------------------------------------
+// Transition Comparator (TCMP)
+//--------------------------------------------------------
+
+module TCMP(clk, rst, a, s);
+
+    input clk, rst;
+    input a;
+    output reg s;
+
+    reg z;
+
+    always @(posedge clk or posedge rst)
+    begin
+        if(rst)
+        begin
+            s <= 1'b0;
+            z <= 1'b0;
+        end
+        else
+        begin
+            z <= a | z;
+            s <= a ^ z;
+        end
+    end
+
+endmodule
+
+
+//--------------------------------------------------------
+// Carry Save Adder (CSADD)
+//--------------------------------------------------------
+
+module CSADD(clk, rst, x, y, sum);
+
+    input clk, rst;
+    input x, y;
+
+    output reg sum;
+
+    reg sc;
+
+    // Half Adder 1
+    wire hsum1, hco1;
+    assign hsum1 = y ^ sc;
+    assign hco1  = y & sc;
+
+    // Half Adder 2
+    wire hsum2, hco2;
+    assign hsum2 = x ^ hsum1;
+    assign hco2  = x & hsum1;
+
+    always @(posedge clk or posedge rst)
+    begin
+        if(rst)
+        begin
+            sum <= 1'b0;
+            sc  <= 1'b0;
+        end
+        else
+        begin
+            sum <= hsum2;
+            sc  <= hco1 ^ hco2;
+        end
+    end
+
+endmodule
